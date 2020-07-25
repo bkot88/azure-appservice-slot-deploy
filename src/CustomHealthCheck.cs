8dc01b09b5b70918b4d0556877d5e7b36c471aae
bkoteski <bkoteski88@gmail.com>
2020-07-25 10:39:46 +1000

    init

---
+++ src/CustomHealthCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthChecks.WebApp
+{
+    public class CustomHealthCheck : IHealthCheck
+    {
+        public CustomHealthCheck()
+        {
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return HealthCheckResult.Degraded();
+        }
+    }
+}
